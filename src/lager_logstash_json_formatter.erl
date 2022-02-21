0a709d8f56e4dd33aa2ed0a35d512d40078c38d8
Bernard Duggan <bernard@circl.es>
2022-02-21 11:41:11 +1100

    Fix handling for OTP24 line/col data

--- src/lager_logstash_json_formatter.erl
+++ src/lager_logstash_json_formatter.erl
@@ -52,15 +52,16 @@ timestamp({Date, Time}) -> [Date, $T, Time].
 convert(Data) -> lists:foldl(fun convert/2, [], Data).
 
 convert({_, undefined}, Acc) -> Acc;
-convert({pid, Pid}, Acc) when is_pid(Pid) ->
-    [{pid, list_to_binary(pid_to_list(Pid))} | Acc];
 convert({K, Pid}, Acc) when is_pid(Pid) ->
     [{K, list_to_binary(pid_to_list(Pid))} | Acc];
 convert({K, List}, Acc) when is_list(List) ->
     [{K, iolist_to_binary(List)} | Acc];
 convert({K, Atom}, Acc) when is_atom(Atom) ->
     [{K, atom_to_binary(Atom, latin1)} | Acc];
-convert(Else, Acc) -> [Else | Acc].
+convert({line, {Line, Col}}, Acc) when is_integer(Line) andalso is_integer(Col) ->
+    [{line, Line} | Acc];
+convert(Else, Acc) ->
+    [Else | Acc].
 
 encode(jsx, Data)   -> jsx:encode(Data);
 encode(jiffy, Data) -> jiffy:encode({Data}).
